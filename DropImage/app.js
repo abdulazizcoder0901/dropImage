81370e5600e215d2bb8e301660c5ac7293ff96c1
Abdulazizcoder0901 <117140464+abdulazizcoder0901@users.noreply.github.com>
2023-06-03 14:23:17 +0500

    Add files via upload

---
+++ DropImage/app.js
@@ -0,0 +1,18 @@
+let boxes = document.querySelectorAll('.box')
+let image = document.querySelector('.image')
+
+boxes.forEach((box) =>{
+    box.addEventListener('dragover', (e) =>{
+        e.preventDefault()
+        box.classList.add('hovered')
+    })
+
+    box.addEventListener('dragleave', () =>{
+        box.classList.remove('hovered')
+    })
+
+    box.addEventListener('drop', () =>{
+        box.appendChild(image)
+        box.classList.remove('hovered')
+    })
+})
